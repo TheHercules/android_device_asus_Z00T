ca5fc66c3a2067a339de4d6a09b23c189f3fca5a
TheHercules <abhijio082@gmail.com>
2017-05-13 09:12:38 +0000

    Desolation-ify device tree

--- deso.mk
+++ deso.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/asus/Z00T/full_Z00T.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Desolation stuff.
+$(call inherit-product, vendor/deso/common.mk)
 
-PRODUCT_NAME := lineage_Z00T
+PRODUCT_NAME := deso_Z00T
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00T
